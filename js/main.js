fa26543423bb61ba1e1376f9b6726cc0f06615c0
unknown <aashusmaileforever@gmail.com>
2018-08-19 14:21:53 +0530

    update the simon game

--- js/main.js
+++ js/main.js
@@ -1,68 +1,129 @@
 $(document).ready(function () {
-  function simonApp() {
-    var red_audio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
-    var green_audio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
-    var yellow_audio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
-    var blue_audio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
-    var squence = [];
-    var counter = 0;
-    var count_2 = 0;
-    var count = document.getElementsByClassName("count");
 
 
-    function nextSquence() {
+  var randomButton = document.querySelectorAll('.random');
+  var counter = document.querySelector('.counter');
 
-      var audios = [red_audio, blue_audio, green_audio, yellow_audio];
-      var randomNumber = audios[Math.floor(Math.random() * audios.length)];
-      squence.push(randomNumber);
-      console.log("the new buttons random is  :" + randomNumber);
-    }
 
-    $("#start_game").on("change", function () {
-      nextSquence();
 
-      function getColors(sound) {
-        if (!squence.length) {
-          nextSquence();
-        } else {
-          if (sound === squence[counter]) {
-            // next step
-            if (counter === squence.length - 1) {
-              counter = 0;
-              nextSquence();
-            } else {
-              counter++;
-              if (counter == 20) {
-                alert("you won the match");
-              }
-            }
-          } else {
-            // lose condition
-            alert("this is wrong");
-            counter = 0;
-            squence = [];
-            window.location.reload(1);
-          }
-        }
+  // sound 
+
+  var simonSound = [
+    "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3",
+    "https://s3.amazonaws.com/freecodecamp/simonSound2.mp3",
+    "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3",
+    "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3",
+  ]
+
+
+  // variables
+
+  var userSeq = [];
+  var simonSeq = [];
+  var numOfLevels = 3;
+  var level = 0;
+  var id = 0;
+  var color = 0;
+
+  document.getElementById('start').addEventListener("click", startGame);
+
+  function startGame(e) {
+    console.log(e)
+    level++;
+    simonSequence();
+
+    $('.randomBtn').click(function () {
+      id = $(this).attr('id');
+      color = $(this).attr('class');
+      userSeq.push(id);
+      console.log(id + " " + color);
+      addClassSound(id, color);
+
+      // checking user squence
+      if (!checkUserSeq()) {
+        displayError();
+        userSeq = [];
+
       }
-
-      $("#red_btn").on("click", function () {
-        getColors(red_audio.play());
-      });
-
-      $("#blue_btn").on("click", function () {
-        getColors(blue_audio.play());
-      });
-
-      $("#yellow_btn").on("click", function () {
-        getColors(yellow_audio.play());
-      });
-
-      $("#green_btn").on("click", function () {
-        getColors(green_audio.play());
-      });
-    });
+      if (userSeq.length == simonSeq.length) {
+        level++;
+        userSeq = [];
+        simonSequence();
+      }
+    })
   }
 
-  setInterval(simonApp, 1000);
-}); // close for document objects notations
+  // checking user seq against simon
+
+  function checkUserSeq() {
+    for (var i = 0; i < userSeq.length; i++) {
+      if (userSeq[i] != simonSeq[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // displaError function
+
+  function displayError() {
+    counter.textContent = "Error";
+    console.log("error");
+    var counting = 0;
+    var myError = setInterval(function () {
+      counting++;
+      if (counting == 3) {
+        counter.textContent = level;
+        clearInterval(myError);
+        userSeq = [];
+        counting = 0;
+      }
+
+    })
+  }
+
+
+
+  //  simon sequence  
+  function simonSequence() {
+    console.log(level);
+    counter.textContent = level;
+    getRandome();
+    var i = 0;
+
+    var myInterval = setInterval(function () {
+      id = simonSeq[i];
+      color = $("#" + id).attr('class');
+      console.log(id + " " + color);
+      addClassSound(id, color);
+      i++;
+      if (i == simonSeq.length) {
+        clearInterval(myInterval);
+      }
+    }, 1000);
+  }
+
+
+  // genrete random number
+
+  function getRandome() {
+    var randomNum = Math.floor(Math.random() * 4);
+    simonSeq.push(randomNum);
+  }
+
+  // add class sound temprary
+
+  function addClassSound(id, color) {
+    playSound()
+    $("#" + id).addClass(color + "-active");
+    setTimeout(function () {
+      $("#" + id).removeClass(color + "-active");
+    }, 500)
+  }
+
+  function playSound(id) {
+
+  }
+
+
+})
